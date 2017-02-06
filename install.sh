554bd681d870b49527aee1cc697ecc1b3b998198
Hallez <jbhallez@aol.com>
2017-02-06 09:22:17 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # Use only if you need to perform changes on the user system such as installing software
 [[ -z $(which mpg321) ]] && sudo apt-get install mpg321
+[[ -z $(which wget) ]] && sudo apt-get install wget
