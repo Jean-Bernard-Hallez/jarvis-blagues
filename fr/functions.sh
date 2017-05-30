e94aaf5f70c841e2b796e5d3afb52ae27bfdd187
Hallez <jbhallez@aol.com>
2017-05-30 14:25:06 +0200

    ATTENTION: Superbe prononciation avec Speech synthesis=google (je n'ai pas testé avec les autres système de synthétiseur vocal) (02/05/17 Co
    You: racontes moi une blague
    jarvis: - Une jeune fille confie à son amie :
    jarvis: - Mon rêve, ce serait d'épouser un archéologue.
    jarvis: - Ah bon ? Et pourquoi ?
    jarvisla: - Parce que plus on vieillit, plus il vous aime.
    
    You: racontes une blague
    jarvis: Quelle est la différence entre une gendarmerie et de la lessive
    .
    jarvis: Dans la lessive il y a au moins trois agents actifs
    
    You: racontes moi une blague
    jarvis: Qu'est ce qu'une blonde intelligente ?
    jarvis: Un labrador.
    
    You: répète la dernière blague
    jarvis: Qu'est ce qu'une blonde intelligente ?
    jarvis: Un labrador.
    
    You: retiens la dernière blague
    jarvis: J'ai sauvegardé la denière blague dans toto 5
    
    You: j'ai combien de blagues sauvegardées
    jarvis:Tu as sauvegardé 8 blagues
    
    You: lit la blague Numéro 4
    jarvis:Voici la blague numéro 4 sauvegardé:
    jarvis:Voici la blague numéro 4 sauvegardé:
    jarvis:- Qu'est-ce que les enfants usent le plus à l'école ?
    jarvis:- Les professeurs !

--- fr/functions.sh
+++ fr/functions.sh
@@ -161,6 +161,9 @@ transcritblague() {
 blaguesaymajuscule=""
 lignesay=$(sed -n "$debutlignealireblague p" $varchemblague)
 direblaguesayok=`echo $lignesay | sed -e "s/<.*/ /g" | sed -e "s/&quot;/ /g" | sed -e "s/&#146;/'/g" | sed -e "s/qu' un/qu'un/g" | sed -e "s/&acirc;/â/g" |  sed -e "s/&ugrave;/ù/g" |sed -e "s/&ucirc;/û/g" | sed -e "s/&eacute;/é/g" | sed -e "s/&Eacute;/é/g" | sed -e "s/&egrave;/è/g" | sed -e "s/&icirc;/î/g" | sed -e "s/&agrave;/à/g" | sed -e "s/&ecirc;/ê/g" | sed -e "s/&euml;/ë/g" | sed -e "s/&#8217;/'/g" | sed -e "s/&ccedil;/ç/g" | sed -e "s/&Ccedil;/ç/g" | sed -e "s/&rsquo;/'/g" | sed -e "s/&ocirc;/ô/g" | sed -e "s/&amp;/et/g" | sed -e "s/ &laquo;/ /g" | sed -e "s/ &raquo;/ /g" | sed -e "s/-\./-/g"`
+direblaguesayok=`echo $direblaguesayok| sed -e "s/&Ecirc;/ /Ê" | sed -e "&Egrave;/ /È" | sed -e "&Ugrave;/ /Ù" | sed -e "&Agrave;/ /À" | sed -e "s/&Acirc;/ /Â" | sed -e "s/&Icirc;/ /Î"`
+
+
 #  &#146;
 }
 
